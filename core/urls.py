4c3e0c16c55d68330564f5daea5f7e70404d625e
FaithKwamboka <okongofaith3@gmail.com>
2022-06-10 19:37:35 +0300

    Feat: Add project urls

--- core/urls.py
+++ core/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('awwwards.urls')),
+    path('accounts/', include('registration.backends.simple.urls')),
+    path('tinymce/', include('tinymce.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('logout/', auth_views.logout_then_login),
 ]
