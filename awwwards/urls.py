39d90cab0d97515d1a1df83978c50e86cd5d0bd1
FaithKwamboka <okongofaith3@gmail.com>
2022-06-11 11:40:11 +0300

    Feat: Update urls

--- awwwards/urls.py
+++ awwwards/urls.py
@@ -3,7 +3,8 @@ from . import views
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-# router.register('users', views.UserViewSet)
+router.register('users', views.UserViewSet)
+router.register('posts', views.PostViewSet)
 router.register('profile', views.ProfileViewSet)
 
 
