236bf81d36c6877077b8e83502e9e33bcbcfd979
FaithKwamboka <okongofaith3@gmail.com>
2022-06-11 11:28:51 +0300

    Feat: Update index view

--- awwwards/views.py
+++ awwwards/views.py
@@ -1,19 +1,46 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
-from models import *
-from serializers import * 
+from .models import *
+from .serializers import * 
 from rest_framework import viewsets
+from .forms import *
+import random
 
 # Create your views here.
-@login_required(login_url='/accounts/login')
-def index (request):
-    return render(request,'index.html')
+def index(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+    else:
+        form = PostForm()
+
+    try:
+        posts = Post.objects.all()
+        posts = posts[::-1]
+        a_post = random.randint(0, len(posts)-1)
+        random_post = posts[a_post]
+        print(random_post.photo)
+    except Post.DoesNotExist:
+        posts = None
+    return render(request, 'index.html', {'posts': posts, 'form': form, 'random_post': random_post})
 
 
 class ProfileViewSet(viewsets.ModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
+    
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
 
 @login_required(login_url='/accounts/login')
 def profile(request, username):
