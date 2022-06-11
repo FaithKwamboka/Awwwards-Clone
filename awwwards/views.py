fdcca8f37c85ab0028da470540a360ba0fcf0609
FaithKwamboka <okongofaith3@gmail.com>
2022-06-11 10:41:19 +0300

    Feat: Add profile views

--- awwwards/views.py
+++ awwwards/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
+from models import *
+from serializers import * 
+from rest_framework import viewsets
 
 # Create your views here.
+@login_required(login_url='/accounts/login')
+def index (request):
+    return render(request,'index.html')
+
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+
+@login_required(login_url='/accounts/login')
+def profile(request, username):
+    return render(request, 'profile.html')
+
+
+def user_profile(request, username):
+    user_prof = get_object_or_404(User, username=username)
+    if request.user == user_prof:
+        return redirect('profile', username=request.user.username)
+    params = {
+        'user_prof': user_prof,
+    }
+    return render(request, 'userprofile.html', params)
+
+
+    
