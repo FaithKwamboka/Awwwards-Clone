ac78d900de27e07190c9c6110d2f0e3ca1459d0f
FaithKwamboka <okongofaith3@gmail.com>
2022-06-11 11:22:00 +0300

    Feat: Add post and user serilizers

--- awwwards/serializers.py
+++ awwwards/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Profile, Post
+from .models import *
 from django.contrib.auth.models import User
 
 
@@ -7,5 +7,18 @@ class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = ['name', 'profile_picture', 'bio', 'location', 'contact']
+        
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['id', 'title', 'url', 'description', 'technologies', 'photo', 'date', 'user']
 
 
+class UserSerializer(serializers.ModelSerializer):
+    profile = ProfileSerializer(read_only=True)
+    posts = PostSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'url', 'username', 'profile', 'posts']
