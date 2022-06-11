32c538d8660db5f8ab7c290b86a2f0f76df23888
FaithKwamboka <okongofaith3@gmail.com>
2022-06-11 11:37:15 +0300

    Feat: Add edit profile view

--- awwwards/views.py
+++ awwwards/views.py
@@ -57,7 +57,28 @@ def user_profile(request, username):
     }
     return render(request, 'userprofile.html', params)
 
-@login_required(login_url='login')
+
+@login_required(login_url='/accounts/login')
+def edit_profile(request, username):
+    user = User.objects.get(username=username)
+    if request.method == 'POST':
+        user_form = UpdateUserForm(request.POST, instance=request.user)
+        prof_form = UpdateUserProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and prof_form.is_valid():
+            user_form.save()
+            prof_form.save()
+            return redirect('profile', user.username)
+    else:
+        user_form = UpdateUserForm(instance=request.user)
+        prof_form = UpdateUserProfileForm(instance=request.user.profile)
+    params = {
+        'user_form': user_form,
+        'prof_form': prof_form
+    }
+    return render(request, 'edit.html', params)
+
+
+@login_required(login_url='/accounts/login')
 def project(request, post):
     post = Post.objects.get(title=post)
     ratings = Rating.objects.filter(user=request.user, post=post).first()
